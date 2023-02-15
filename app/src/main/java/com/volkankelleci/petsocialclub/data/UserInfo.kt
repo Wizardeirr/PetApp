e65473f9aeee1171fcf8b1e37bb6e3f1f2c686ec
Volkan Kelleci <kellecivolkan@gmail.com>
2023-02-16 02:01:49 +0300

    Parameters fixed

--- app/src/main/java/com/volkankelleci/petsocialclub/data/UserInfo.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/data/UserInfo.kt
@@ -1,5 +1,12 @@
 package com.volkankelleci.petsocialclub.data
 
-class UserInfo(var uuid:String,var userEmail:String, var userName:String, var petName:String,var userImage:String,var userPassword:String) {
+class UserInfo(
+    var uuid: String,
+    var userEmail: String,
+    var userName: String,
+    var petName: String,
+    var userImage: String,
+    var userPassword: String
+) {
 
 }
