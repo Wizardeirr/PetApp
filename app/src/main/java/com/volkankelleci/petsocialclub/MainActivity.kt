9416e9c14f0cc3e33e46fc6fdeae1b329001e0f6
Volkan Kelleci <kellecivolkan@gmail.com>
2023-03-02 13:56:53 +0300

    Splash screen will add(than earthquake )

--- app/src/main/java/com/volkankelleci/petsocialclub/MainActivity.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.volkankelleci.petsocialclub
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import com.volkankelleci.petsocialclub.R
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+
+                Handler().postDelayed(Runnable { //This method will be executed once the timer is over
+                    // Start your app main activity
+                    val i = Intent(this@MainActivity, MainActivity::class.java)
+                    startActivity(i)
+                    // close this activity
+                    finish()
+                }, 3000)
+            }
+        }
 
     }
 
