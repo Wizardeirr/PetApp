5932c01ced77bdb0daa22c3560f4c7b4a9951058
Volkan Kelleci <kellecivolkan@gmail.com>
2023-03-05 18:33:17 +0300

    NEW dATA BASE CREATED

---
+++ app/src/main/java/com/volkankelleci/petsocialclub/data/LastMessageDataBase.kt
@@ -0,0 +1,9 @@
+package com.volkankelleci.petsocialclub.data
+
+class LastMessageDataBase(
+    var lastMessage:String,
+    var lastMessagefromUUID:String,
+    var lastMessagetoUUID:String,
+    var lastMessagetimestamp: String,
+    var lastMessagechatUser:String) {
+}
