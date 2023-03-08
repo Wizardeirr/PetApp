b46262c0dfe837391e1ee1e8919e27f0e60609ba
Volkan Kelleci <kellecivolkan@gmail.com>
2023-03-08 16:14:47 +0300

    debugging on PrivateChatFragment.kt

--- app/src/main/java/com/volkankelleci/petsocialclub/privatemessage/PrivateChatFragment.kt
+++ app/src/main/java/com/volkankelleci/petsocialclub/privatemessage/PrivateChatFragment.kt
@@ -79,7 +79,7 @@ class PrivateChatFragment : Fragment() {
                 if(dy <-10 && !downForNewMessageForPM.isShown){
                     downForNewMessageForPM.visibility=View.VISIBLE
                 }
-                if (!userChatRV.canScrollVertically(-1)&&!userChatRV.canScrollVertically(+1)){
+                if (!privateMessageRV.canScrollVertically(-1)&&!privateMessageRV.canScrollVertically(+1)){
                     downForNewMessageForPM.visibility=View.VISIBLE
                 }
 
